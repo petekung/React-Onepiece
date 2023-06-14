7b54e445be0f483f507386b5f7d4a0c5966b6e19
petekung2000 <peach@yojoies.com>
2023-06-14 16:53:38 +0700

    #

--- src/componenct/DataOnepiece.jsx
+++ src/componenct/DataOnepiece.jsx
@@ -5,62 +5,15 @@ import { MagnifyingGlass } from 'react-loader-spinner'
 import { Table } from 'antd';
 const url = 'https://api.api-onepiece.com/characters';
 
-
-
-// axios.get(url).then((response) => {
-//   const Data_1 = response.data 
-//   Data_1.forEach((itme,index) => {
-//        data.push({
-//         key: index,
-//         name: itme.french_name,
-//       });
-//       console.log(itme);
-//   })
-
-
- 
-  // })
 function DataOnepiece() { 
-  const data = [];
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex:'name',
-    },
-  ];
-  data.push({
-    key: "1",
-    name: "asdasd",
-  });
-  const [charactersAll, setCharactersAll] = useState([])
-  const [loading, setLoading] = useState(1)
-  const fetachDataOnepiece = async ()=>{
-  try{
-    setLoading(true)
-    const response = await axios.get(url)
-    const Data_1 = response.data 
-    Data_1.forEach((itme,index) => {
-             data.push({
-              key: index,
-              name: itme.french_name,
-            });
-            console.log(itme);
-        })
-    console.log(data);
-  } catch(error){
-    console.log("error");
-  } finally{
-    setLoading(false);
-  }
 
-
-}
+const [data, setData] = useState([])
 
 
  
 
 
-
+  
 
 
 
@@ -69,11 +22,8 @@ function DataOnepiece() {
     <>
       <div style={{ display: 'flex', justifyContent: "center", textAlign: "center", width: "90%", margin: "0 auto" }} >
         <div style={{ width: "100%" }}>
-            <div> 
-                <button onClick={fetachDataOnepiece}>Get DATA </button>
-
-            </div>
-          <Table columns={columns} dataSource={data} />
+     
+         {/* // <Table columns={columns} dataSource={data} /> */}
 
         </div>
 
