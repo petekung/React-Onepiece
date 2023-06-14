2dd1232b55cfde7e18355f30e1375204bfe7233b
petekung2000 <peach@yojoies.com>
2023-06-14 18:04:03 +0700

    #

--- src/componenct/DataOnepiece.jsx
+++ src/componenct/DataOnepiece.jsx
@@ -3,60 +3,62 @@ import React from 'react'
 import axios from 'axios';
 import { MagnifyingGlass } from 'react-loader-spinner'
 import { Table } from 'antd';
-const url = 'https://api.api-onepiece.com/characters';
-axios.get(url).then((response) => {
-  var Data = response.data
-  console.log("DATA",Data);
-})
-  .catch((error) => {
-  });
-
 function DataOnepiece() {
   const [data, setData] = useState([])
+  const columns = [
+    {
+      key: "1",
+      title: 'Name',
+      dataIndex: 'name',
+      width: "100px"
+    },
+    {
+      key: "2",
+      title: 'Bounty',
+      dataIndex: 'bounty',
+      width: "100px"
+    },
+  ];
+  const DataALL = [];
+  data.forEach((item) => {
+    DataALL.push({
+      key: item.id,
+      name: item.french_name,
+      bounty: item.bounty,
+    });
+  });
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    getData();
+  }, [])
+  const getData = () => {
+    const url = 'https://api.api-onepiece.com/characters';
+    axios.get(url).then((response) => {
+      var Data = response.data
+      setData(Data)
+      if (Data) {
+        setLoading(true)
+      }
 
+    })
+      .catch((error) => {
+      }
+      );
+
+  }
   return (
     <>
       <div style={{ display: 'flex', justifyContent: "center", textAlign: "center", width: "90%", margin: "0 auto" }} >
-        <div style={{ width: "100%" }}>
 
-          {/* // <Table columns={columns} dataSource={data} /> */}
-          <div>11</div>
-        </div>
+        {loading ? <div style={{ width: "50%" }}>
 
+          <Table columns={columns} dataSource={DataALL} />
+        </div> : <MagnifyingGlass
+
+        />}
       </div>
 
     </>
-    // <div style={{ display: 'flex', justifyContent: "center", textAlign: "center", width: "90%", margin: "0 auto" }}>
-    //   <table >  {loading ? <tr>
-    //     <th style={{ width: "100px" }}>Name</th>
-
-    //     <th style={{ width: "100px" }} >Bounty</th>
-    //   </tr> : <MagnifyingGlass
-    //     visible={true}
-    //     height="80"
-    //     width="80"
-    //     ariaLabel="MagnifyingGlass-loading"
-    //     wrapperStyle={{}}
-    //     wrapperClass="MagnifyingGlass-wrapper"
-    //     glassColor='#c0efff'
-    //     color='#e15b64'
-    //   />}
-
-
-
-
-    //     {charactersAll.map(All => (
-
-    //       <tr>
-    //         <td>{All.french_name}</td>
-
-    //         <td>{All.bounty}</td>
-
-    //       </tr>
-    //     ))}
-    //   </table>
-
-    // </div>
   )
 }
 
